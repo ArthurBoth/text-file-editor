18106e9a9a2297741aa1ef5db64c5fc378974c8f
Arthur Both <arthur.both001@edu.pucrs.br>
2024-06-02 19:27:41 -0300

    Added support for cleansing presets
    Added a ModifierType file that allows coding cleansing preset.
    Added RegEx constants to help with the presets.
    Added the .Input and .Output folders to the repo.

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
+import modification.Cleanser;
+
+import static modification.ModifierType.TRANSCRIPTION;
+import static modification.ModifierType.CENSOR;
 
 public class App{
     public static void main(String[] args) {
-        // Cleanser.clearFile("example.txt", "\\d+");      // ignore
-        // Cleanser.clearFolder("\\d+");                   // ignore
+        Cleanser.clearFile("example.txt", TRANSCRIPTION);
+        Cleanser.clearFolder(CENSOR);
     }
 }
---
+++ src/constants/FolderPaths.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class FolderPaths {
+    public static final String INPUT_FOLDER = "./.Input/";
+    public static final String OUTPUT_FOLDER = "./.Output/";
+
+    private FolderPaths() {
+        throw new IllegalStateException("Utility class");
+    }
+}
